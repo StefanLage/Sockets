d67ddf678ec36eb10ca455c3d7bbcc1489b7100b
Stefan Lage <lagestfan@gmail.com>
2013-07-13 12:00:00 -0400

    Defining a protocol

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,12 @@
-from twisted.internet.protocol import Factory
+from twisted.internet.protocol import Factory, Protocol
 from twisted.internet import reactor
 
+class Chat(Protocol):
+	def connectionMade(self):
+		print "Someone is connected"
+
 factory = Factory()
+factory.protocol = Chat
 reactor.listenTCP(80, factory)
+print "Connection opened"
 reactor.run()
