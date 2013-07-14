201bc63462f8f06d7fe233997f73e468ccc50c9f
Stefan Lage <lagestfan@gmail.com>
2013-07-14 17:49:11 -0400

    Managing Chat events

--- Server/server.py
+++ Server/server.py
@@ -8,7 +8,24 @@ class Chat(Protocol):
 	def connectionLost(self, reason):
 		self.factory.clients.remove(self)
 		print "A client is deconnected"
+	def dataReceived(self, data):
+		d_array = data.split(':')
+		if len(d_array) > 1:
+			command = d_array[0]
+			content = d_array[1]
+			msg = ""
+			if command == "iam":
+				self.name = content
+				msg = self.name + " has joined"
+			elif command == "msg":
+				msg = self.name + ": " + content
+				
+			print msg
 
+			for c in self.factory.clients:
+				c.message(msg)
+	def message(self, msg):
+		self.transport.write(msg + '\n')
 
 factory = Factory()
 factory.protocol = Chat
