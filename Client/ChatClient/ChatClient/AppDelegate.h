e44b7dff4d2abd64885127ce5d0360eeeac18d83
Stefan Lage <lagestfan@gmail.com>
2013-07-15 06:47:32 -0400

    Init an iOS client

---
+++ Client/ChatClient/ChatClient/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ChatClient
+//
+//  Created by Stefan Lage on 7/15/13.
+//  Copyright (c) 2013 Stefan Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Client/ChatClient/ChatClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChatClient
+//
+//  Created by Stefan Lage on 7/15/13.
+//  Copyright (c) 2013 Stefan Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
